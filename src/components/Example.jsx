c55b9e37dee0d9c4ca6d4f3e23396c1b2731898e
khawlaboulahrouf <khawlaboulahrouf@gmail.com>
2026-03-06 09:53:37 +0000

    Initialisation du projet et structure modulaire ES6

--- src/components/Example.jsx
+++
@@ -1,9 +0,0 @@
-function Example(){
-    return(
-        <div>
-            <h1>app</h1>
-        </div>
-    )
-}
-
-export default Example;
---
+++ src/data/tournamentDB.js
@@ -0,0 +1,18 @@
+export const tournaments = [
+  {
+    id: 1,
+    title: "Shuttle Masters League",
+    sport: "Badminton",
+    status: "On Going",
+    date: "October 12, 2025",
+    location: "Badminton Hall 1"
+  },
+  {
+    id: 2,
+    title: "Hoops Championship",
+    sport: "Basketball",
+    status: "Upcoming",
+    date: "November 2, 2025",
+    location: "Indoor Arena"
+  }
+];
